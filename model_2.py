f055f08bb188f2b61642ecde687531f4745e407a
Kavin <kavinanand77@gmail.com>
2020-07-16 16:50:27 -0700

    turned off batch

--- model_2.py
+++ model_2.py
@@ -14,41 +14,41 @@ def unet(pretrained_weights = None,input_size = (256,256,2), activation = 'relu'
     
     inputs = Input(input_size)
     conv1 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(inputs)
-    conv1 = BatchNormalization()(conv1)
+    #conv1 = BatchNormalization()(conv1)
     conv1 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    conv1 = BatchNormalization()(conv1)
+    #conv1 = BatchNormalization()(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
     layers *= 2
 
     conv2 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    conv2 = BatchNormalization()(conv2)
+    #conv2 = BatchNormalization()(conv2)
     conv2 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    conv2 = BatchNormalization()(conv2)
+    #conv2 = BatchNormalization()(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
     layers *= 2
 
     conv3 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    conv3 = BatchNormalization()(conv3)
+    #conv3 = BatchNormalization()(conv3)
     conv3 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    conv3 = BatchNormalization()(conv3)
+    #conv3 = BatchNormalization()(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
     layers *= 2
 
     conv4 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(pool3)
-    conv4 = BatchNormalization()(conv4)
+    #conv4 = BatchNormalization()(conv4)
     conv4 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    conv4 = BatchNormalization()(conv4)
+    #conv4 = BatchNormalization()(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
     layers *= 2
 
     conv5 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(pool4)
-    conv5 = BatchNormalization()(conv5)
+    #conv5 = BatchNormalization()(conv5)
     conv5 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    conv5 = BatchNormalization()(conv5)
+    #conv5 = BatchNormalization()(conv5)
 
     layers //= 2
 
@@ -56,9 +56,9 @@ def unet(pretrained_weights = None,input_size = (256,256,2), activation = 'relu'
     #up6 = BatchNormalization()(up6)
     merge6 = concatenate([conv4,up6], axis = 3)
     conv6 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(merge6)
-    conv6 = BatchNormalization()(conv6)
+    #conv6 = BatchNormalization()(conv6)
     conv6 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv6)
-    conv6 = BatchNormalization()(conv6)
+    #conv6 = BatchNormalization()(conv6)
 
     layers //= 2
 
@@ -66,9 +66,9 @@ def unet(pretrained_weights = None,input_size = (256,256,2), activation = 'relu'
     #up7 = BatchNormalization()(up7)
     merge7 = concatenate([conv3,up7], axis = 3)
     conv7 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(merge7)
-    conv7 = BatchNormalization()(conv7)
+    #conv7 = BatchNormalization()(conv7)
     conv7 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv7)
-    conv7 = BatchNormalization()(conv7)
+    #conv7 = BatchNormalization()(conv7)
 
     layers //= 2
 
@@ -76,9 +76,9 @@ def unet(pretrained_weights = None,input_size = (256,256,2), activation = 'relu'
    # up8 = BatchNormalization()(up8)
     merge8 = concatenate([conv2,up8], axis = 3)
     conv8 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(merge8)
-    conv8 = BatchNormalization()(conv8)
+    #conv8 = BatchNormalization()(conv8)
     conv8 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv8)
-    conv8 = BatchNormalization()(conv8)
+    #conv8 = BatchNormalization()(conv8)
 
     layers //= 2
     
@@ -86,15 +86,16 @@ def unet(pretrained_weights = None,input_size = (256,256,2), activation = 'relu'
     #up9 = BatchNormalization()(up9)
     merge9 = concatenate([conv1,up9], axis = 3)
     conv9 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    conv9 = BatchNormalization()(conv9)
+    #conv9 = BatchNormalization()(conv9)
     conv9 = Conv2D(layers, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = BatchNormalization()(conv9)
+    #conv9 = BatchNormalization()(conv9)
     
     # Don't run this
     #conv9 = Conv2D(2, 3, activation = activation, padding = 'same', kernel_initializer = 'he_normal')(conv9)
     #conv9 = BatchNormalization()(conv9)
 
     conv10 = Conv2D(2, 1, activation='linear')(conv9)
+    #conv10 = BatchNormalization()(conv10)
 
     model = Model(inputs,conv10)
 
